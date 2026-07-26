dca4d757296b49c9b881a00ff8805eeeab33768e
VISHAL R S <vishalrs2466@gmail.com>
2026-07-26 23:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,14 +1,26 @@
 class Solution {
     public int climbStairs(int n) {
+
+        // If n is 0 or 1, there is only 1 way to reach the top
         if (n == 0 || n == 1) {
             return 1;
         }
+
         int prev = 1, curr = 1;
+
+        // Start from 2 because we already know the ways to reach step 0 and step 1
         for (int i = 2; i <= n; i++) {
+
             int temp = curr;
+
+         // Number of ways to reach the current step = ways to reach the previous step + ways to reach two steps before
             curr = prev + curr;
+
+            // Move prev forward for the next iteration
             prev = temp;
         }
+
+        // curr contains the total number of ways to reach step n
         return curr;
     }
 }
