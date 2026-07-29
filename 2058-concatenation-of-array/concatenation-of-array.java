d6c62d62ad30c83ae0fd31818fd2b7d7ef57a30e
VISHAL R S <vishalrs2466@gmail.com>
2026-07-29 22:10:43 +0530

    Time: 1 ms (97.60%) | Memory: 47.2 MB (37.23%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int[] ans =new int[2*n];
+        for (int i=0 ; i<n ; i++) //Error possibility i<2*n is wrong since nums is of length n it can't expand since there is no elements after nin nums
+        { 
+            ans[i]=nums[i];//1st half
+            ans[i+n] = nums[i];//2nd half
+        }
+        return ans;
+    }
+}
