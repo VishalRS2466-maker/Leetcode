1ff789136784755e4947f4a857d365a1ec6a2656
VISHAL R S <vishalrs2466@gmail.com>
2026-07-30 22:06:01 +0530

    Time: 1 ms (97.62%) | Memory: 46.9 MB (90.33%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -10,3 +10,13 @@ class Solution {
         return ans;
     }
 }
+
+/* O(n)  class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        System.arraycopy(nums, 0, ans, 0, nums.length);
+        System.arraycopy(nums, 0, ans, nums.length, nums.length);
+        return ans;
+    }
+}   */        
+            
