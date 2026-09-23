c6982c7a459b39535eaae484ca532a6dfe4f579a
VISHAL R S <vishalrs2466@gmail.com>
2026-09-23 11:24:51 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (38.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -15,20 +15,21 @@ class Solution {
         int carry=0;
         while(l1 != null || l2 !=null || carry!=0)
         {   
-            int sum=carry;//if sum of values is double digit
-          
+            int sum=carry;//carry from the previous sum is added to sum
            if(l1 != null)
            {
             sum+=l1.val;
             l1=l1.next;
            }
-           if(l2 != null)//use if not else cause l2 should also be added 
+           if(l2 != null)//use "if" not "else" cause l2 should also be added 
            {
             sum+=l2.val;
             l2=l2.next;
            }
-            carry = sum / 10;
+            carry = sum / 10;//check carry for the updated sum
             curr.next = new ListNode(sum % 10);
+            //can't store the rem as int directly so convert it into a node
+
            curr=curr.next;
         }
         return add.next;
