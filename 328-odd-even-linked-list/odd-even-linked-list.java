29c2f07128e0ca58461047b3a417ef75ff6818a5
VISHAL R S <vishalrs2466@gmail.com>
2026-09-23 12:29:59 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (88.54%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+
+        // If list is empty or has only one node
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+
+            // odd indices
+            odd.next = even.next;
+            odd = odd.next;
+
+            // even indices
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        // Connect odd list with even list
+        odd.next = evenHead;
+
+        return head;
+    }
+}
